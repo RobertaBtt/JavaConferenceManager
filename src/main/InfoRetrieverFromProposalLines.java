15b860f95685bcd617870d8698feb1c2444d0ce0
Roby <roberta.btt@gmail.com>
2017-06-03 11:17:14 +0200

    Added test to compute the minutes of every proposal, the total lenght of the sum of proposals, in terms of minutes and days.

---
+++ src/main/InfoRetrieverFromProposalLines.java
@@ -0,0 +1,70 @@
+package main;
+
+import java.util.List;
+
+public class InfoRetrieverFromProposalLines {
+	
+	private  static final String LIGHTNING_STRING = "lightning";
+	private  static final String LUNCH_STRING = "Lunch";
+	private  static final String NETWORKING_EVENT_STRING = "Networking Event";
+	
+	private  static final int LIGHTNING = 5;
+	private  static final int LUNCH = 60;
+	private  static final int NETWORKING_EVENT = 60;
+	
+	private static final double MAX_DURATION_PER_DAY = 420.0;
+	
+	public static int getMinutes(String proposalLine){
+		return getNumber(proposalLine);
+		
+	}
+	
+	
+
+	private static int getNumber(String proposal){
+		
+		int number = 0;
+		
+		String[] split = proposal.split(" ");		
+		String lastElement = split[split.length - 1];
+				
+		if (lastElement.equalsIgnoreCase(LIGHTNING_STRING))
+			number = LIGHTNING;
+		
+		else if (lastElement.equalsIgnoreCase(LUNCH_STRING))
+			number = LUNCH;
+		
+		else{
+			if (proposal.equals(NETWORKING_EVENT_STRING))
+				number = NETWORKING_EVENT;
+			
+			else
+				number = Integer.valueOf(lastElement.substring(0,2));			
+		}
+		
+		return number;
+		
+	}
+	
+	public static int getTotalMinutes(List<String> proposals){
+		
+		int minutes = 0;
+		for (int i = 0; i < proposals.size(); i++) {
+			minutes += InfoRetrieverFromProposalLines.getMinutes(proposals.get(i));
+		}
+		return minutes;
+	}
+	
+	
+	public static int getNumberOfDays(List<String> proposals){
+		
+		double dividedResult = InfoRetrieverFromProposalLines.getTotalMinutes(proposals) / MAX_DURATION_PER_DAY;
+		
+		if (dividedResult - (int)(dividedResult) >0)
+			return (int) (dividedResult + 1); //day after
+		
+		else return (int)dividedResult;
+		
+	}
+	
+}
--- src/test/InfoRetrieverFromProposalLineTest.java
+++ src/test/InfoRetrieverFromProposalLineTest.java
@@ -1,15 +1,28 @@
 package test;
-import main.ProposalOrdererDivideIntoSessions;
 
 import static org.junit.Assert.*;
 import org.junit.*;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.FileScanner;
+import main.InfoRetrieverFromProposalLines;
+import main.ProposalOrdererDivideIntoSessions;
 
 
-public class ProposalOrdererDivideIntoSessionsTest {
+public class InfoRetrieverFromProposalLineTest {
 	
 	private List<String> proposals = new ArrayList<String>();
-	private ProposalOrdererDivideIntoSessions proposalOrderer;
+	
+	private  static final String PROPOSAL_1 = "Writing Fast Tests Against Enterprise Rails 60min";
+	private  static final String PROPOSAL_2 = "Rails for Python Developers lightning";
+	private  static final int MIN_60 = 60;
+	private  static final int LIGHTNING = 5;
+	private  static final int LUNCH = 60;
+	private  static final int NETWORKING_EVENT = 60;
+	private  static final int TOTAL_MINUTES = 785;
+	private  static final int NUMBER_OF_DAYS = 2;
 	
 	@Before
 	public void setUp() throws Exception {
@@ -33,25 +46,30 @@ public class ProposalOrdererDivideIntoSessionsTest {
 		proposals.add("Ruby on Rails Legacy App Maintenance 60min");
 		proposals.add("A World Without HackerNews 30min");
 		proposals.add("User Interface CSS in Rails Apps 30min");
-		
-		
-		proposalOrderer = new ProposalOrdererDivideIntoSessions();
-		
+				
+	}
+	
+	@Test
+	public final void testProposal1(){
+		assertEquals(MIN_60, InfoRetrieverFromProposalLines.getMinutes(PROPOSAL_1));		
+	}
+	
+	@Test
+	public final void testProposal2(){
+		assertEquals(LIGHTNING, InfoRetrieverFromProposalLines.getMinutes(PROPOSAL_2));		
 	}
 
 	@Test
-	public final void testListOrdererImplementationObjectExists(){
-		assertNotNull(proposalOrderer);		
+	public final void testTotalMinutes(){
+		assertEquals(TOTAL_MINUTES, InfoRetrieverFromProposalLines.getTotalMinutes(proposals));		
 	}
-
-	
 	
 	@Test
-	public final void testOrderList(){
-		proposals = proposalOrderer.getReordererList(proposals);
-		assertEquals("09:00AM Writing Fast Tests Against Enterprise Rails 60min", proposals.get(0));
-
-	}
+	public final void testNumberOfDays(){
+		assertEquals(NUMBER_OF_DAYS, InfoRetrieverFromProposalLines.getNumberOfDays(proposals));		
+	} 
+	
+	
 	
 	
 }
